71f9b62f8e544328c1ca4e539757409d2aae7b21
maxim <akimov0603@mail.ru>
2022-09-03 22:25:35 +0500

    Первая задача из Домашнего Задания

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("введите число");
+
+string numberStr = Console.ReadLine();
+int a = int.Parse(numberStr);
+Console.WriteLine("введите число");
+
+string numberst =(Console.ReadLine());
+int b=int.Parse(numberst);
+if (a<b)
+{
+    Console.Write("Первое меньше второго");
+}
+else if (a>b)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
