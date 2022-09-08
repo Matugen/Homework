d94ab75e0375373d74d17944af5746b58e86a607
maxim <akimov0603@mail.ru>
2022-09-08 20:14:56 +0500

    задача на нахождение макс числа из трех

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+
+int max= a;
+if (b>a); max=b;
+if (c>max); max=c;
+
+
+Console.WriteLine("Максимальное число");
+Console.WriteLine(max);
+
+
+
