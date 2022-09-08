9417e1b142ff64fa82782eb4c36cb99f29d9729b
maxim <akimov0603@mail.ru>
2022-09-08 20:17:34 +0500

    Задача на нахожд макс числа из трех чисел

---
+++ Program copy.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int a = int.Parse(numberStr);
+Console.WriteLine("Введите число");
+string numberSt = Console.ReadLine();
+int b = int.Parse(numberSt);
+if (a<b)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+else if (a>b)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+
+
+
+
+
