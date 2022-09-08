148dc980bfaea1bea1ca4a177e2b20904b131a11
maxim <akimov0603@mail.ru>
2022-09-08 21:37:03 +0500

    Задача на определение четного или нечетного числа

---
+++ HomeWork2/NewProject/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine());
+if (a%2 == 0);
+{
+        Console.WriteLine("Было введено четное число");
+
+}
+else
+{
+    Console.WriteLine("Было введено нечетное число");
+}
